088b6e4cb19bf36b4df04e3295e3a0f86941844e
Siilveira <goncalocardoso.silveira@gmail.com>
2025-04-10 18:03:27 +0100

    expense

--- dotnetwebapp/obj/Debug/net9.0/dotnetwebapp.AssemblyInfo.cs
+++ dotnetwebapp/obj/Debug/net9.0/dotnetwebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetwebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d536489cb2aeb8f68d058cb925c5f16163900506")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetwebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetwebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
